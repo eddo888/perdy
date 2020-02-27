7d17c8a11b070ebcd73c3cf0da63b65d00e5f7ac
David Edson <eddo888@tpg.com.au>
2020-02-27 21:36:18 +1100

    scripts

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pwd = path.abspath(path.dirname(__file__))
 with codecs.open(path.join(pwd, 'README.md'), 'r', encoding='utf8') as input:
     long_description = input.read()
 
-version='1.3'
+version='1.4'
 	
 setup(
 	name='Perdy',
@@ -31,4 +31,9 @@ setup(
         'argcomplete',
 		'Baubles',
 	],
+	scripts=[
+		'bin/parser.py',
+		'bin/pyson.py',
+	],
+)
 )
