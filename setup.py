e9ac128b258cdf91f0f6c22c81e8ee9dcd9e09f3
David Edson <dave@stibosystems.com.au>
2020-03-18 11:20:05 +1100

    with jsonpath

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pwd = path.abspath(path.dirname(__file__))
 with codecs.open(path.join(pwd, 'README.md'), 'r', encoding='utf8') as input:
     long_description = input.read()
 
-version='1.6'
+version='1.7'
 	
 setup(
 	name='Perdy',
@@ -28,6 +28,7 @@ setup(
 		'arrow',
 		'xmltodict',
 		'PyYAML',
+		'jsonpath',
         'argcomplete',
 		'Baubles',
 	],
