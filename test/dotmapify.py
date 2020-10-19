efe9e29db290ac116b24d1098a38f061289183ba
David Edson <eddo888@tpg.com.au>
2020-10-19 21:01:56 +1100

    dotmap aware

---
+++ test/dotmapify.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os, re, sys, json
+
+from dotmap import DotMap
+
+sys.path.insert(0, '..')
+from Perdy.pretty import prettyPrintLn
+
+doc = DotMap(dict(root='opml', nodes=[]))
+
+prettyPrintLn(doc)
+	
